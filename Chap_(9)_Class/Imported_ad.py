211a882f3d2e218fc672c271c9cfa7f6df82f06c
Mahmud Mahmud Ibrahim <123310933+Mudacrixxz@users.noreply.github.com>
2023-02-23 01:22:32 +0100

    correction

--- Chap_(9)_Class/Imported_ad.py
+++ Chap_(9)_Class/Imported_ad.py
@@ -1,5 +1,6 @@
 """
-9-11. Imported Admin: Start with your work from Exercise 9-8 (page 173).
+Question (9-11)
+Imported Admin: Start with your work from Exercise 9-8 (page 173).
 Store the classes User, Privileges, and Admin in one module. Create a sepa￾rate file, make an Admin instance, and call show_privileges() to show that
 everything is working correctly.
 """
